15f067a4042dff74b088af555e8e0170a1613a74
Joe <lmcs121005@gmail.com>
2023-08-05 13:58:40 +0800

    Now mode 9 of the control panel will remove all related tickets

--- src/admin.py
+++ src/admin.py
@@ -326,6 +326,14 @@ def adminMode() -> None:
 				print(f"Clearing all seat of house {house_num}")
 				house.clearPlan()
 				print("Success!")
+				print("fClearing all related tickets")
+				n_tickets_removed: int = 0
+				for ticket in House.tickets_table:
+					ticket_no, time, house_no, movie, row_index, column_index = ticket
+					if house_no == house.house_number:
+						House.tickets_table.remove(ticket)
+						n_tickets_removed += 1
+				print(f"Removed {n_tickets_removed} tickets")
 				saveData()
 				continue
 			else:
