0a6d58abd0a146cfb1149902043524e2b8cb9c60
Joe <lmcs121005@gmail.com>
2023-08-06 16:18:31 +0800

    `-l`,`-c`, `--license`, ``--copyright` arguments

--- __main__.py
+++ __main__.py
@@ -97,7 +97,27 @@ if __name__ == '__main__':
 		epilog="For documentation of the usage of this program, visit https://joeccp.github.io/SBA/",
 		usage="SBA"
 	)
-	parser.parse_args()
+	parser.add_argument("--license", "-l", "--copyright", "-c",
+	                    help="Print copyright information",
+	                    action='store_true')
+	args = parser.parse_args()
+	if args.license or args.copyright:
+		print("""
+		Copyright 2023 Joe Chau
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.""")
+		quit()
+	
 	
 	
 	# main is called INSIDE this if-statement,
