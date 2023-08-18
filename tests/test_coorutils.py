02fb3611ab9ebe62d6ddcfae4e003d719610ea08
Joe <lmcs121005@gmail.com>
2023-08-18 20:53:42 +0800

    Range checks for unit testing

--- tests/test_coorutils.py
+++ tests/test_coorutils.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from src.coorutils import *
+from ..src.coorutils import *
 from typing import Callable
 from unittest import TestCase
 
@@ -31,6 +31,15 @@ class Test_coorExprAnalysis(TestCase):
 		self.assertEqual(analysis('Z99'),[(98, 25)])
 		self.assertEqual(analysis('17M'), [(16, 12)])
 		self.assertEqual(analysis('o8'), [(7, 14)])
+		self.assertEqual(analysis('23Q', n_row=84), [(22, 16)])
+		self.assertEqual(analysis('Q90', n_row=90), [(89, 16)])
+		self.assertEqual(analysis('1z', n_row=1), [(0, 25)])
+		self.assertEqual(analysis('23Q', n_column=25), [(22, 16)])
+		self.assertEqual(analysis('23Q', n_column=17), [(22, 16)])
+		self.assertEqual(analysis('1A', n_column=1), [(0, 0)])
+		self.assertEqual(analysis('23Q', n_column=84), [(22, 16)])
+		self.assertEqual(analysis('23Q', n_row=84, n_column=98), [(22, 16)])
+
 
 	
 	def test_twoCoordinates(self):
@@ -43,7 +52,7 @@ class Test_coorExprAnalysis(TestCase):
 		self.assertTrue(analysis('8C:74I'), [(7, 2), (73, 8)])
 		
 
-	def test_stringForamtting(self):
+	def test_stringFormatting(self):
 		self.assertEqual(analysis('    1 a    '), [(0, 0)])
 		self.assertEqual(analysis('  A  1: B2  '), [(0, 0), (1, 1)])
 	
@@ -72,7 +81,7 @@ class Test_coorExprAnalysis(TestCase):
 		with self.assertRaises(ValueError):
 			analysis("A1", n_row=12345, n_column=4)
 	
-	def test_invalidSyntax(self):
+	def test_singleInvalidSyntax(self):
 		with self.assertRaises(EmptyCoordinate):
 			analysis('')
 		with self.assertRaises(InvalidCharacter):
@@ -99,3 +108,51 @@ class Test_coorExprAnalysis(TestCase):
 			analysis('Q55Q')
 		with self.assertRaises(ColumnCoordinatesAtTwoSide):
 			analysis('JFDK324jpd345dsfk')
+		with self.assertRaises(AlphabetCharacterInRowNumber):
+			analysis('A1B2')
+		with self.assertRaises(AlphabetCharacterInRowNumber):
+			analysis('C11RR22')
+		with self.assertRaises(MoreThanOneColon):
+			analysis('7Y::6C')
+		with self.assertRaises(MoreThanOneColon):
+			analysis('42B:99K:6C')
+		
+		
+	def test_multipleSelection(self):
+		...
+	
+	
+	def test_rangeCheck(self):
+		with self.assertRaises(SameCoordinates):
+			analysis('35P:35P')
+		with self.assertRaises(SameCoordinates):
+			analysis('P35:P35')
+		with self.assertRaises(SameCoordinates):
+			analysis('35P:P35')
+		with self.assertRaises(SameCoordinates):
+			analysis('p45:45P')
+		with self.assertRaises(CoordinatesWrongOrder):
+			analysis('78T:32D')
+		with self.assertRaises(CoordinatesWrongOrder):
+			analysis('99C:98F')
+		with self.assertRaises(CoordinatesWrongOrder):
+			analysis('32D:u18')
+		with self.assertRaises(CoordinatesWrongOrder):
+			analysis('1b:1A')
+		with self.assertRaises(CoordinatesWrongOrder):
+			analysis('w87:87h')
+		with self.assertRaises(RowNumberOutOfRange):
+			analysis('99Z', n_row=13)
+		with self.assertRaises(RowNumberOutOfRange):
+			analysis('36c', n_row=23, n_column=9)
+		with self.assertRaises(RowNumberOutOfRange):
+			analysis('1A:56L', n_row=35)
+		with self.assertRaises(RowNumberOutOfRange):
+			analysis('23d:34k', n_row=1, n_column=1)
+		with self.assertRaises(ColumnNumberOutOfRange):
+			analysis('1a:99Z', n_column=23)
+		with self.assertRaises(ColumnNumberOutOfRange):
+			analysis('23o', n_column=14)
+		with self.assertRaises(ColumnNumberOutOfRange):
+			analysis('78q', n_column=10, n_row=87)
+		
