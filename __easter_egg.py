7e99200e9e9eedbc39900841e46a40f33a569897
Joe <lmcs121005@gmail.com>
2023-08-31 18:43:46 +0800

    formatting of comments

--- __easter_egg.py
+++ __easter_egg.py
@@ -1,4 +1,4 @@
-# flake8: noqa
+# flake8: NOQA
 exec(__import__(bytes.fromhex('626173653634').decode(
 		bytes.fromhex('6173636969').decode(bytes.fromhex('7574662d38').decode()))).b64decode(
 		('cHJpbnQoJycuam9pbihbY2hyKGkpIGZvciBpIGluIFsyNywgOTEsIDU3LCA1OSwgNTMsIDEwOSwgNzQsIDE'
