7ec85f595ef2bb1819ba5db82e149dc87b713208
Joe <lmcs121005@gmail.com>
2023-09-30 21:35:38 +0800

    More details for the setup file

--- setup.py
+++ setup.py
@@ -17,4 +17,34 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-	setup()
+	setup(
+		name="SBA",
+		author="Joe Chau",
+		author_email="lmcs121005@gmail.com",
+		description="A Cinema kiosk system for HKDSE ICT SBA project.",
+		long_description="This is a Cinema Kiosk System project for "
+		                 "the Hong Kong Diploma of Secondary Education Examination 2024 "
+		                 "Information and Communication Technology "
+		                 "Elective Part D - Software Development School-based Assessment. "
+		                 "This project is a simple simulation of a cinema movie ticket system. "
+		                 "This Cinema Kiosk System provides an easy way for customers to buy movie tickets. "
+		                 "It also serves the administrator as a control platform."
+		                 "Visit https://joeccp.github.io/SBA for more details.",
+		url="https://github.com/Joeccp/SBA",
+		classifiers=[  # https://pypi.org/classifiers/
+			"Environment :: Console",
+			"Environment :: Win32 (MS Windows)",
+			"Intended Audience :: Customer Service",
+			"License :: OSI Approved :: Apache Software License",
+			"Natural Language :: Chinese (Traditional)",
+			"Natural Language :: English",
+			"Operating System :: Microsoft :: Windows :: Windows 10",
+			"Operating System :: Microsoft :: Windows :: Windows 11",
+			"Programming Language :: Python :: 3.11",
+			"Programming Language :: Python :: 3.12",
+			"Programming Language :: Python :: 3.13",
+			"Topic :: Games/Entertainment :: Simulation",
+			"Topic :: Software Development :: Embedded Systems",
+			"Topic :: Software Development :: User Interfaces",
+		]
+	)
