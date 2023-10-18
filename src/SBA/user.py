b5a90068bb8777eb8ba4f05ab0a68ef125e97579
JoeGor <71358024+Joeccp@users.noreply.github.com>
2023-10-18 11:35:10 +0800

    Fix the sorting

--- src/SBA/user.py
+++ src/SBA/user.py
@@ -235,7 +235,7 @@ def buyTicket() -> None:
 		# Remove repeated
 		selected_seat_list_no_repeat: set[tuple[int, int]] = set(selected_seat_list)
 		selected_seat_list: list[tuple[int, int]] = list(selected_seat_list_no_repeat)
-		selected_seat_list: list[tuple[int, int]] = sorted(selected_seat_list, key=lambda tup: tup[0])
+		selected_seat_list: list[tuple[int, int]] = sorted(selected_seat_list, key=lambda tup: tup)
 	
 		if len(selected_seat_list) > total_ticket_number:
 			if language == 'ENGLISH':
